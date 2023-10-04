daa644c4016cf7be7ff31a4c3e87a1240939385b
Arthur Givigir <arthur316@gmail.com>
2023-10-04 17:00:46 -0300

    🐛 Fix init not public

--- Sources/InfiniteLoop/Extensions/View+Modifiers.swift
+++ Sources/InfiniteLoop/Extensions/View+Modifiers.swift
@@ -22,6 +22,11 @@ public extension View {
 public struct RoundedCorner: Shape {
     var radius: CGFloat = .infinity
     var corners: UIRectCorner = .allCorners
+    
+    public init(radius: CGFloat, corners: UIRectCorner) {
+        self.radius = radius
+        self.corners = corners
+    }
 
     public func path(in rect: CGRect) -> Path {
         let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
